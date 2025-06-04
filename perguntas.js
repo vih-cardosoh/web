befd41f598b3805a7743b2b92e97f67997299558
vih-cardosoh <cardoso.maria25@escola.pr.gov.br>
2025-06-04 10:19:23 -0300

    salvar

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O que é um mineral?',
+    'Um mineral é uma substância natural, inorgânica e sólida, com uma estrutura cristalina definida e uma composição química característica. Eles são formados por processos geológicos e são a base das rochas. Exemplos incluem quartzo, feldspato e mica.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'História',
+    'Em que ano o livro "O Principe", de Maquiavel foi publicado?',
+    'O livro de Maquiavel foi publicado em 1535'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quando é usado o ING?',
+    'O "ing" em inglês é usado em várias situações, principalmente para formar verbos contínuos (como o presente contínuo, passado contínuo, etc.), para formar substantivos (gerúndios) e também como adjetivo. Além disso, é usado após preposições e em algumas expressões específicas.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a função do retículo endoplasmático rugoso?',
+    'A função do retículo endoplasmatico é a produção de proteínas'
 )
